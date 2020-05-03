22c210504cd61de38db14f92287f2ea1510fd461
Daria <dariaurbanik@gmail.com>
2020-05-03 17:05:05 +0200

    Initial commit

---
+++ src/CaesarCipher.java
@@ -0,0 +1,26 @@
+public class CaesarCipher {
+    public String encrypt(String input, int key){
+        //Make a StringBuilder with message (encrypted)
+        StringBuilder encrypted = new StringBuilder(input);
+        //Write down the alphabet
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //Compute the shifted alphabet
+        String shiftedAlphabet = alphabet.substring(key) + alphabet;
+
+        //Count from 0 to < length of encrypted (call it i))
+        for (int i = 0; i < encrypted.length(); i++){
+            //Look at the ith character of encrypted (call it currChar)
+            char currChar = encrypted.charAt(i);
+            //Find the index of currChar in the alphabet (call it idx)
+            int idx = alphabet.indexOf(currChar);
+            //If currChar is in the alphabet
+                if (idx != -1){
+                    //Get the idxth character of shiftedAlphabet (newChar)
+                    char newChar = shiftedAlphabet.charAt(idx);
+                    //Replace the ith character of encrypted with newChar
+                    encrypted.setCharAt(i, newChar);
+                }
+        }
+        return encrypted.toString();
+    }
+}
